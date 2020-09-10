06c40f65b9d502491ba1fed79560c3626bc2a282
mykyta-butsenko <70696257+mykyta-butsenko@users.noreply.github.com>
2020-09-10 14:22:10 +0300

    Create main.cs
    10/09/2020 STRYKER BY BUTSENKO

---
+++ main.cs
@@ -0,0 +1,10 @@
+class Program  
+{  
+    static void Main(String[] args)
+    {  
+        var stryker = "STRYKER!!!";
+        var stryker2 = "STRYKER2";
+        var stryker3 = "STRYKER3";
+        Console.WriteLine("PHYSIO-CONTROL IS THE BEST STRYKER IN THE ENTIRE LIFENET STRYKERABITITY")
+    } 
+}
